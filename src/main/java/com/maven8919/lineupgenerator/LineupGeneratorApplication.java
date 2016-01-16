ac8b580ec9cdac5a0cf7b085445c223ae5c03367
maven8919 <maven8919@gmail.com>
2016-01-16 13:04:43 +0100

    Initial commit

---
+++ src/main/java/com/maven8919/lineupgenerator/LineupGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.maven8919.lineupgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineupGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineupGeneratorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/maven8919/lineupgenerator/LineupGeneratorApplicationTests.java
@@ -0,0 +1,18 @@
+package com.maven8919.lineupgenerator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = LineupGeneratorApplication.class)
+@WebAppConfiguration
+public class LineupGeneratorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
